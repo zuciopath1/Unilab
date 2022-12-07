3be9a52ff8f3528ffddc7b2c8b5a8cea2bcdc50a
zuciopath <73157609+zuciopath@users.noreply.github.com>
2022-12-07 20:12:56 +0400

    .

---
+++ app.js/main.js
@@ -0,0 +1,27 @@
+const menu = document.querySelector(".burgerMenu");
+const menuItems = document.querySelectorAll(".burgerItems");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+
+hamburger.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+      menu.classList.remove("showMenu");
+      closeIcon.style.display = "none";
+      menuIcon.style.display = "block";
+    } else {
+      menu.classList.add("showMenu");
+      closeIcon.style.display = "block";
+      menuIcon.style.display = "none";
+    }
+  }
+
+menuItems.forEach( 
+    function(menuItem) { 
+      menuItem.addEventListener("click", toggleMenu);
+    }
+  )
+// =============================================
